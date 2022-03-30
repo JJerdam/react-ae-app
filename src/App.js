d078c9e435793aeafd41f48e906aa4a1a0011fdf
Michał Jerominek <michal.jerominek@monterail.com>
2022-03-30 09:34:46 +0200

    react-ae output init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import * as React from "react";
 import {Helmet} from 'react-helmet';
 import './App.css';
 
+
+
 function App() {
+  const handleRef = (ref) => {
+    ref.addEventListener('formSubmitted', (ev) => {
+      console.log("Event:", ev)
+    })
+  }
   return (
       <div className="App">
         <Helmet>
           <script src="./my-form.js" type="text/javascript" />
         </Helmet>
         <h3>Angular Web Component in action</h3>
-        <lib-header></lib-header>
-        <my-custom-form></my-custom-form>
+        {/* <lib-header></lib-header> */}
+        <my-custom-form ref={handleRef}></my-custom-form>
       </div>
   );
 }
